3e6189b2861d3da59d02ba50e302726cd903f79b
Arman Yessenamanov <yesenarman@gmail.com>
2021-07-13 02:48:04 +0300

    Add workload

--- worker.ts
+++ worker.ts
@@ -1,7 +1,22 @@
-addEventListener('fetch', (event) => {
-  event.respondWith(new Response('Hello Deno Deploy', {
-    headers: {
-      'content-type': 'text/plain'
-    }
-  }));
+addEventListener("fetch", (event) => {
+  const startTime = performance.now();
+  const iterations = Math.floor(Math.random() * 1e9);
+
+  let sum = 0;
+  for (let i = 1; i <= iterations; ++i) {
+    sum += i * i;
+  }
+
+  const elapsedTime = performance.now() - startTime;
+
+  event.respondWith(
+    new Response(
+      `Completed ${iterations} in ${elapsedTime / 1000} seconds: sum = ${sum}`,
+      {
+        headers: {
+          "content-type": "text/plain",
+        },
+      },
+    ),
+  );
 });
